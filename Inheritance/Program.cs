f875b2f879b30769807269180359b896afa15099
Shullyd7 <65193001+Shullyd7@users.noreply.github.com>
2021-12-10 23:55:42 +0100

    Add files via upload

---
+++ Inheritance/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Inheritance
+{
+    class Chairman : ManagingDirector
+    {
+        //string nameOfCompany;
+
+  
+        static void Main(string[] args)
+        {
+            Chairman chairman = new Chairman();
+            chairman.firstname = "Jacob";
+            chairman.surname = "Adams";
+            chairman.age = 40;
+            chairman.gender = "Male";
+            chairman.phoneNumber = "09087645624";
+            chairman.nameOfCompany = "Microsoft";
+           
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
